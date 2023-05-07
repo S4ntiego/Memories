d9bdbc855f91d0b138e5b640261f9b821c565932
S4ntiego <adamksiazek123@gmail.com>
2023-05-07 12:52:44 +0200

    cleaning of console logs

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
-import { AUTH } from '../constants/actionTypes';
-import * as api from '../api/index.js';
+import { AUTH } from "../constants/actionTypes";
+import * as api from "../api/index.js";
 
 export const signin = (formData, router) => async (dispatch) => {
   try {
@@ -7,9 +7,9 @@ export const signin = (formData, router) => async (dispatch) => {
 
     dispatch({ type: AUTH, data });
 
-    router.push('/');
+    router.push("/");
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
@@ -19,8 +19,8 @@ export const signup = (formData, router) => async (dispatch) => {
 
     dispatch({ type: AUTH, data });
 
-    router.push('/');
+    router.push("/");
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,5 +1,17 @@
-import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE, COMMENT, FETCH_BY_CREATOR } from '../constants/actionTypes';
-import * as api from '../api/index.js';
+import {
+  START_LOADING,
+  END_LOADING,
+  FETCH_ALL,
+  FETCH_POST,
+  FETCH_BY_SEARCH,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+  COMMENT,
+  FETCH_BY_CREATOR,
+} from "../constants/actionTypes";
+import * as api from "../api/index.js";
 
 export const getPost = (id) => async (dispatch) => {
   try {
@@ -9,43 +21,52 @@ export const getPost = (id) => async (dispatch) => {
 
     dispatch({ type: FETCH_POST, payload: { post: data } });
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
 export const getPosts = (page) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING });
-    const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
+    const {
+      data: { data, currentPage, numberOfPages },
+    } = await api.fetchPosts(page);
 
-    dispatch({ type: FETCH_ALL, payload: { data, currentPage, numberOfPages } });
+    dispatch({
+      type: FETCH_ALL,
+      payload: { data, currentPage, numberOfPages },
+    });
     dispatch({ type: END_LOADING });
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
 export const getPostsByCreator = (name) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING });
-    const { data: { data } } = await api.fetchPostsByCreator(name);
+    const {
+      data: { data },
+    } = await api.fetchPostsByCreator(name);
 
     dispatch({ type: FETCH_BY_CREATOR, payload: { data } });
     dispatch({ type: END_LOADING });
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING });
-    const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
+    const {
+      data: { data },
+    } = await api.fetchPostsBySearch(searchQuery);
 
     dispatch({ type: FETCH_BY_SEARCH, payload: { data } });
     dispatch({ type: END_LOADING });
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
@@ -58,7 +79,7 @@ export const createPost = (post, history) => async (dispatch) => {
 
     history.push(`/posts/${data._id}`);
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
@@ -68,19 +89,19 @@ export const updatePost = (id, post) => async (dispatch) => {
 
     dispatch({ type: UPDATE, payload: data });
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
 export const likePost = (id) => async (dispatch) => {
-  const user = JSON.parse(localStorage.getItem('profile'));
+  const user = JSON.parse(localStorage.getItem("profile"));
 
   try {
     const { data } = await api.likePost(id, user?.token);
 
     dispatch({ type: LIKE, payload: data });
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
@@ -92,7 +113,7 @@ export const commentPost = (value, id) => async (dispatch) => {
 
     return data.comments;
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
@@ -102,6 +123,6 @@ export const deletePost = (id) => async (dispatch) => {
 
     dispatch({ type: DELETE, payload: id });
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
